6ca556f9e51f946660af48345728a7a8a96f29ef
Shaheer707 <minhasshaheer0@gmail.com>
2023-07-18 19:20:20 +0500

    Initial-commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -46,7 +46,7 @@ const Contact = () => {
           ref={formRef}
           onSubmit={handleSubmit}
           className='mt-12 flex flex-col gap-8'
-          netlify
+          data-netlify="true"
         >
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Your Name</span>
