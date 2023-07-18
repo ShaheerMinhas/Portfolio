80742c7179a612818bdaf54865e3921bdfdd284b
Shaheer707 <minhasshaheer0@gmail.com>
2023-07-18 18:56:29 +0500

    Initial-commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-           Rubix Code &nbsp;
+           Rubix Code woww&nbsp;
             <span className='sm:block hidden'> | Software Dev Company</span>
           </p>
         </Link>
