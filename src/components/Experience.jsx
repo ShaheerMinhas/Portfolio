383bd3d80f146c8f0d03b5b57448890f0cc1a580
Shaheer707 <minhasshaheer0@gmail.com>
2023-08-07 19:55:04 +0500

    initial

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React,{Component} from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
 import { motion } from "framer-motion";
-import Slider from "react-slick";
 
+import "react-vertical-timeline-component/style.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "react-vertical-timeline-component/style.min.css";
-
 import { styles } from "../styles";
+
 import { experiences } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { textVariant } from "../utils/motion";
-import PhotoCard from "../components/PhotoCard.jsx";
-
+import PhotoCard  from "../components/PhotoCard.jsx";
+import Carousel, { autoplayPlugin } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
+import { slidesToShowPlugin } from "@brainhubeu/react-carousel";
 const Data = [
   {
     name: "Shaheer Minhhas",
@@ -19,7 +24,7 @@ const Data = [
     imageUrl: "pictures/shaheer.jpg",
     linkedin: "https://www.linkedin.com/in/shaheer-minhas-86a108231/",
     github: "https://github.com/ShaheerMinhas",
-
+   
     insta: ""
   },
   {
@@ -36,16 +41,16 @@ const Data = [
     imageUrl: "pictures/romesa.jpeg",
     linkedin: "https://www.linkedin.com/in/romisha-2326801a4/",
     github: "",
-
+  
     insta: ""
   },
   {
     name: "Ahsan Naveed",
     position: "Designer",
-    imageUrl: "pictures/ahsan2.jpg",
+    imageUrl: "pictures/ahsan2.JPG",
     linkedin: "https://www.linkedin.com/in/ahsan-n-805a5a230/",
     github: "",
-
+   
     insta: ""
   },
   {
@@ -54,38 +59,52 @@ const Data = [
     imageUrl: "pictures/abbas.jpg",
     linkedin: "https://www.linkedin.com/company/rubix-code/about/",
     github: "",
-
+   
     insta: ""
   }
 ]
 
 const Experience = () => {
-  // Settings for the carousel
-  const carouselSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3, // Show three team members at a time
-    slidesToScroll: 3, // Scroll three team members at a time
-  };
 
   return (
+   
     <>
-      <div className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}>
+      <div
+        className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
+      >
         <motion.div variants={textVariant()}>
           <p className={styles.sectionSubText}>MEET OUR TEAM</p>
           <h2 className={styles.sectionHeadText}>TEAM</h2>
         </motion.div>
-        <div className="flex flex-wrap border-box h-auto justify-center items-center container">
-          <Slider {...carouselSettings}>
-            {Data.map((member, index) => (
+        <Carousel
+          plugins={[
+            'infinite',
+            'arrows',
+            {
+              
+              resolve:  autoplayPlugin,
+              options: {
+                interval: 2000, 
+                numberOfSlides: 3
+              }
+            },
+            
+          ]}
+          animationSpeed={1000}
+        >
+        
+          {
+            Data.map((member, index) => (
               <PhotoCard key={index} {...member} />
-            ))}
-          </Slider>
-        </div>
+              
+            ))
+          }
+      
+        </Carousel>
       </div>
     </>
   );
+             
 };
 
 export default SectionWrapper(Experience, "work");
