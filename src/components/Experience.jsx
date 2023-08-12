df02321687c13b4905e5107a56e4be95ee9a2e14
Shaheer707 <minhasshaheer0@gmail.com>
2023-08-13 03:47:38 +0500

    initial

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,21 +1,26 @@
+import React,{Component} from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
 
-
-
-import React from "react";
-import Carousel, { autoplayPlugin } from "@brainhubeu/react-carousel";
-import "@brainhubeu/react-carousel/lib/style.css";
-import TeamInfo from "./TeamInfo";
-import PhotoCard from "../components/PhotoCard";
-import { textVariant } from "../utils/motion";
-import { SectionWrapper } from "../hoc";
+import "react-vertical-timeline-component/style.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { styles } from "../styles";
-import { motion } from "framer-motion"; // Make sure to import this if not already imported
 
-// Rest of the imports remain the same
-
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
+import PhotoCard  from "../components/PhotoCard.jsx";
+import Carousel, { autoplayPlugin } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
+import { slidesToShowPlugin } from "@brainhubeu/react-carousel";
+import TeamInfo from "./TeamInfo";
 const Data = [
   {
-    name: "Shaheer Minhas",
+    name: "Shaheer Minhhas",
     position: "Financial Manager",
     imageUrl: "pictures/shaheer.jpg",
     linkedin: "https://www.linkedin.com/in/shaheer-minhas-86a108231/",
@@ -34,17 +39,17 @@ const Data = [
   },
   {
     name: "Romisha Maria",
-    position: "Designer",
+    position: "Social Manager",
     imageUrl: "pictures/romesa.jpeg",
     linkedin: "https://www.linkedin.com/in/romisha-2326801a4/",
     github: "",
     desc: "Meet Romesa She's a talented coder and a valuable member of our new software company  ",
-
+  
     insta: ""
   },
   {
     name: "Ahsan Naveed",
-    position: "Technical Head",
+    position: "Designer",
     imageUrl: "pictures/ahsan2.JPG",
     linkedin: "https://www.linkedin.com/in/ahsan-n-805a5a230/",
     github: "",
@@ -53,59 +58,55 @@ const Data = [
   },
   {
     name: "Abbas Rizvi",
-    position: "Social Media Manager",
+    position: "HR",
     imageUrl: "pictures/abbas.jpg",
     linkedin: "https://www.linkedin.com/company/rubix-code/about/",
     github: "",
     desc: " Meet Abbas! Our brilliant coder and tech enthusiast Stay tuned for some groundbreaking innovations! ",
     insta: ""
   }
-];
+]
 
 const Experience = () => {
+
   return (
-    <div className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>MEET OUR TEAM</p>
-        <h2 className={styles.sectionHeadText}>TEAM</h2>
-      </motion.div>
-      <Carousel
-        plugins={[
-          "infinite",
-          "arrows",
-          {
-            resolve: autoplayPlugin,
-            options: {
-              interval: 2000,
-              numberOfSlides: 1, // Show only one slide at a time on mobile
-            },
-          },
-        ]}
-        animationSpeed={1000}
-        slidesPerPage={1} // Added to ensure one slide per page on mobile
-        breakpoints={{
-          // Adjust these breakpoints based on your design and requirements
-          600: {
-            // Up to 600px screen width (mobile)
-            slidesPerPage: 1,
-            arrows: false, // Hide arrows on mobile
-          },
-          1024: {
-            // Up to 1024px screen width (tablet)
-            slidesPerPage: 3, // Show three slides on tablets
-            arrows: true,
-          },
-        }}
+   
+    <>
+      <div
+        className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
       >
-        {Data.map((member, index) => (
-          <div key={index} className="mb-6 md:flex md:flex-row items-center">
-            <PhotoCard {...member} className="md:mr-4" />
-            <TeamInfo name={member.name} position={member.position} desc={member.desc} />
-          </div>
-        ))}
-      </Carousel>
-    </div>
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>MEET OUR TEAM</p>
+          <h2 className={styles.sectionHeadText}>TEAM</h2>
+        </motion.div>
+        <Carousel
+          plugins={[
+            'infinite',
+            'arrows',
+            {
+              
+              resolve:  autoplayPlugin,
+              options: {
+                interval: 2000, 
+                numberOfSlides: 3
+              }
+            },
+            
+          ]}
+          animationSpeed={1000}
+        >
+          {Data.map((member, index) => (
+            <div key={index} className="team-member-container mb-4 md:flex md:flex-row items-center">
+              <PhotoCard {...member} className="md:mr-4" />
+              <TeamInfo name={member.name} position={member.position} desc={member.desc} />
+            </div>
+          ))}
+      
+        </Carousel>
+      </div>
+    </>
   );
+             
 };
 
 export default SectionWrapper(Experience, "work");
