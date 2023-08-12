9cfbd15ac276fa8653b8fd04d5de49e8e520c872
Shaheer707 <minhasshaheer0@gmail.com>
2023-08-12 19:54:40 +0500

    initial

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -17,6 +17,7 @@ import PhotoCard  from "../components/PhotoCard.jsx";
 import Carousel, { autoplayPlugin } from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 import { slidesToShowPlugin } from "@brainhubeu/react-carousel";
+import TeamInfo from "./TeamInfo";
 const Data = [
   {
     name: "Shaheer Minhhas",
@@ -24,7 +25,7 @@ const Data = [
     imageUrl: "pictures/shaheer.jpg",
     linkedin: "https://www.linkedin.com/in/shaheer-minhas-86a108231/",
     github: "https://github.com/ShaheerMinhas",
-   
+    desc: "Meet Shaheer, the coding virtuoso of our dream team! Say hello to the tech whisperer behind the scenes!",
     insta: ""
   },
   {
@@ -33,7 +34,8 @@ const Data = [
     imageUrl: "pictures/zain1.jpg",
     linkedin: "https://www.linkedin.com/in/zain-asher-3960321b2/",
     github: "",
-    insta: ""
+    insta: "",
+    desc: "Meet Zain, the big mind behind our new software innovations Stay tuned for more exciting updates"
   },
   {
     name: "Romisha Maria",
@@ -41,6 +43,7 @@ const Data = [
     imageUrl: "pictures/romesa.jpeg",
     linkedin: "https://www.linkedin.com/in/romisha-2326801a4/",
     github: "",
+    desc: "Meet Romesa She's a talented coder and a valuable member of our new software company  ",
   
     insta: ""
   },
@@ -50,7 +53,7 @@ const Data = [
     imageUrl: "pictures/ahsan2.JPG",
     linkedin: "https://www.linkedin.com/in/ahsan-n-805a5a230/",
     github: "",
-   
+    desc: " Meet Ahsan- our brilliant coder extraordinaire! Say hello to the tech genius behind our innovative solutions! ",
     insta: ""
   },
   {
@@ -59,7 +62,7 @@ const Data = [
     imageUrl: "pictures/abbas.jpg",
     linkedin: "https://www.linkedin.com/company/rubix-code/about/",
     github: "",
-   
+    desc: " Meet Abbas! Our brilliant coder and tech enthusiast Stay tuned for some groundbreaking innovations! ",
     insta: ""
   }
 ]
@@ -95,7 +98,12 @@ const Experience = () => {
         
           {
             Data.map((member, index) => (
-              <PhotoCard key={index} {...member} />
+              <div key="index" className="flex  ml-2">
+                {/* Apply ml-4 (margin left 1rem) to PhotoCard */}
+                <PhotoCard key={index} {...member} className="ml-10"/>
+                <TeamInfo name={member.name} position ={member.position} desc={member.desc}/>
+              </div>
+              
               
             ))
           }
---
+++ src/components/TeamInfo.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+const TeamInfo = ({ name, position,desc }) => {
+    return (
+        <div className="flex flex-col justify-center ml-4">
+            <h3 className="text-lg  mb-1">{name}</h3>
+            <p className="text-md font-semibold">{position}</p>
+            <p className="text-sm text-gray-600 line-clamp-3">{desc}</p>
+        </div>
+    );
+};
+
+export default TeamInfo;
